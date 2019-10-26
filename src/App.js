6368a9de0ce40e119e71550b78f5c1821fbb2e48
Muhammad Hashmi <hashmim2297@gmail.com>
2019-10-26 01:02:12 -0400

    Added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 import MaterialTable from 'material-table';
 
 export default function MaterialTableDemo() {
@@ -19,39 +19,46 @@ export default function MaterialTableDemo() {
   });
 
   return (
-    <MaterialTable
-      title="Boston Code Camp Rooms"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: newData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data.push(newData);
-              setState({ ...state, data });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data[data.indexOf(oldData)] = newData;
-              setState({ ...state, data });
-            }, 600);
-          }),
-        onRowDelete: oldData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data.splice(data.indexOf(oldData), 1);
-              setState({ ...state, data });
-            }, 600);
-          }),
-      }}
-    />
+    <div className="App">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="table">
+        <MaterialTable
+          title="Boston Code Camp Rooms"
+          columns={state.columns}
+          data={state.data}
+          editable={{
+            onRowAdd: newData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  const data = [...state.data];
+                  data.push(newData);
+                  setState({ ...state, data });
+                }, 600);
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  const data = [...state.data];
+                  data[data.indexOf(oldData)] = newData;
+                  setState({ ...state, data });
+                }, 600);
+              }),
+            onRowDelete: oldData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  const data = [...state.data];
+                  data.splice(data.indexOf(oldData), 1);
+                  setState({ ...state, data });
+                }, 600);
+              }),
+          }}
+        />
+      </div>
+    </div>
   );
 }
