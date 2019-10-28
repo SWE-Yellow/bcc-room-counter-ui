561e99e54a693489fd7479643351350c4affcc04
Muhammad Hashmi <hashmim2297@gmail.com>
2019-10-28 17:32:28 -0400

    added router

--- src/App.js
+++ src/App.js
@@ -26,13 +26,20 @@ export default class App extends React.Component {
     else {
 
       return (
-        <div className="App">
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-          <div className="navbar">
-            <Navbar />
+        <Router>
+          <div className="App">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+            <div className="navbar">
+              <Navbar />
+            </div>
+            <Route path='/' exact strict component={LoginForm}></Route>
+            <Route path='/Rooms' component={Rooms}></Route>
+            <Route path='/Speakers' component={Speakers}></Route>
+            <Route path='/Times' component={Times}></Route>
+            <Route path='/Presentations' component={Presentations}></Route>
           </div>
-        </div>
+        </Router>
       );
     }
   }
