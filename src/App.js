c6961af8b6f51d8ad8634fcf175bd0b2a8b5fb2e
Muhammad Hashmi <hashmim2297@gmail.com>
2019-10-28 17:25:33 -0400

    fixed links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginForm from './components/LoginForm/LoginForm';
 import Rooms from './components/Rooms';
-// import Times from './components/Times';
-// import Speakers from './components/Speakers';
-// import Presentations from './components/Presentations';
+import Speakers from './components/Speakers';
+import Times from './components/Times';
+import Presentations from './components/Presentations';
 
 export default class App extends React.Component {
   state = {
@@ -16,7 +18,18 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        < Rooms />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <Router>
+          < Navbar />
+          <switch>
+              <Route path='/' exact strict component={LoginForm}></Route>
+              <Route path='/Rooms' component={Rooms}></Route>
+              <Route path='/Speakers' component={Speakers}></Route>
+              <Route path='/Times' component={Times}></Route>
+              <Route path='/Presentations' component={Presentations}></Route>
+          </switch>
+        </Router>
       </div>
     );
   }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import { Nav, Navbar }from 'react-bootstrap';
-import { Link, BrowserRouter as Router } from "react-router-dom";
+import './Navbar.css';
 
 const navbar = props => (
-  <Router>
-    <Navbar justify variant="tabs" className="justify-content-center" bg="dark" variant="dark" fixed="bottom" >
-      <Nav variant="pills" defaultActiveKey="Rooms">
-        <Nav.Item>
-          <Nav.Link eventKey="Rooms" exact to="../Rooms">Rooms</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="Speakers" to="../Speakers">Speakers</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="Times" to="../Times">Time Slots</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="Presentations" to="Presentations">Presentations</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Navbar>
-  </Router>
+
+  <Navbar justify className="justify-content-center" bg="dark" variant="dark" fixed="bottom" >
+    <Nav variant="pills" defaultActiveKey="/Rooms">
+      <Nav.Item>
+        <Nav.Link eventKey="/Rooms" exact href='/Rooms'>Rooms</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="/Speakers" exact href='/Speakers'>Speakers</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="/Times" exact href='/Times'>Time Slots</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="/Presentations" exact href='/Presentations'>Presentations</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  </Navbar>
 
 );
 
