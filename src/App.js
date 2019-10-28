3ca0c9413a668cb19f7e1e6632462f4fa349272e
Muhammad Hashmi <hashmim2297@gmail.com>
2019-10-28 13:11:51 -0400

    pages

--- src/App.js
+++ src/App.js
@@ -1,64 +1,24 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import MaterialTable from 'material-table';
+import Rooms from './components/Rooms';
+// import Times from './components/Times';
+// import Speakers from './components/Speakers';
+// import Presentations from './components/Presentations';
 
-export default function MaterialTableDemo() {
-  const [state, setState] = React.useState({
-    columns: [
-      { title: 'Room', field: 'room' },
-      { title: 'Room count', field: 'count' },
-    ],
-    data: [
-      { room: 'Accelerate', count: 100 },
-      {
-        room: 'Dobbs 310',
-        count: 30
-      },
-    ],
-  });
+export default class App extends React.Component {
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
 
-  return (
-    <div className="App">
-      <div className="navbar">
-        <Navbar />
+  render() {
+    return (
+      <div className="App">
+        < Navbar />
+        {/* < Rooms /> */}
       </div>
-      <div className="table">
-        <MaterialTable
-          title="Boston Code Camp Rooms"
-          columns={state.columns}
-          data={state.data}
-          editable={{
-            onRowAdd: newData =>
-              new Promise(resolve => {
-                setTimeout(() => {
-                  resolve();
-                  const data = [...state.data];
-                  data.push(newData);
-                  setState({ ...state, data });
-                }, 600);
-              }),
-            onRowUpdate: (newData, oldData) =>
-              new Promise(resolve => {
-                setTimeout(() => {
-                  resolve();
-                  const data = [...state.data];
-                  data[data.indexOf(oldData)] = newData;
-                  setState({ ...state, data });
-                }, 600);
-              }),
-            onRowDelete: oldData =>
-              new Promise(resolve => {
-                setTimeout(() => {
-                  resolve();
-                  const data = [...state.data];
-                  data.splice(data.indexOf(oldData), 1);
-                  setState({ ...state, data });
-                }, 600);
-              }),
-          }}
-        />
-      </div>
-    </div>
-  );
+    );
+  }
 }
---
+++ src/components/Presentations.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar/Navbar';
+import MaterialTable from 'material-table';
+
+export default function MaterialTableDemo() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Room', field: 'room' },
+      { title: 'Room count', field: 'count' },
+    ],
+    data: [
+      { room: 'Accelerate', count: 100 },
+      {
+        room: 'Dobbs 310',
+        count: 30
+      },
+    ],
+  });
+
+  return (
+    <div className="Speakers">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="table">
+        <MaterialTable
+          title="Boston Code Camp Presentations"
+          columns={state.columns}
+          data={state.data}
+          editable={{
+            onRowAdd: newData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  const data = [...state.data];
+                  data.push(newData);
+                  setState({ ...state, data });
+                }, 600);
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  const data = [...state.data];
+                  data[data.indexOf(oldData)] = newData;
+                  setState({ ...state, data });
+                }, 600);
+              }),
+            onRowDelete: oldData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  const data = [...state.data];
+                  data.splice(data.indexOf(oldData), 1);
+                  setState({ ...state, data });
+                }, 600);
+              }),
+          }}
+        />
+      </div>
+    </div>
+  );
+}
