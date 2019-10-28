96e2d11170e11738af96ca4d212ad0cd7637aa3e
wintleh <hunter.wintle@gmail.com>
2019-10-28 17:10:12 -0400

    Add links for MaterialTable icons

--- src/App.js
+++ src/App.js
@@ -44,6 +44,8 @@ export default class App extends React.Component{
           <div className="navbar">
             <Navbar />
           </div>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
           <div className="table">
             <MaterialTable
               title="Boston Code Camp Rooms"
