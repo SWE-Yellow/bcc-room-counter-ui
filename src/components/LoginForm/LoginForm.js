07dad3327b9da7beecf410e64ef95eb1394461ae
Hunter Wintle <37913095+wintleh@users.noreply.github.com>
2019-10-28 11:16:30 -0400

    Buttons (#2)
    * Added add, delete, and edit button components
    
    * Added login button and updated "styleClass" prop
    
    The property "styleClass" is now className for all buttons
    
    * Added Login component and stylesheet

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,160 @@
+import React from "react";
+import TextField from '@material-ui/core/TextField';
+
+import './LoginForm.css';
+import { LoginButton } from "../buttons";
+
+const initialState = {
+    error: null, 
+    person: {
+        username: "",
+        password: ""
+    }
+};
+
+export default class LoginForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = initialState;
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    render() {
+        // Update the state everytime a value in the usename or password field changes
+        // Also display errors from the state if they exist
+        return (
+            <div id="loginContainer">
+                <div id="loginInterface">
+                    <LoginHeader />
+                    <div id="inputContainer">
+                        <UsernameField onChange={ this.handleChange } /> 
+                        <br/>
+                        <PasswordField onChange={ this.handleChange } />
+                    </div>
+                    <ErrorMessage error={ this.state.error } />
+                    <div id="loginButtonContainer">
+                        <LoginButton className="login" onClick={this.onClick} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    /**
+     * Updates the state based on the event
+     */
+    handleChange(event, newValue) {
+        event.persist(); 
+
+        // Update the state based on the text field that changed
+        this.setState((state) => {
+            // state.person[event.target.name] = newValue; // newValue is undefined?, below line works though
+            state.person[event.target.name] = event.target.value;
+        });
+
+    }
+
+    /**
+     * Verifies values in username and password fields. If there is a problem, update the state with the error.
+     */
+    onClick() {
+/******************************************************************************/
+        if(this.state.person['username'] === "" || this.state.person['password'] === "") {
+            
+            // Have the error message in state, but have no username or password
+            const errorState = {
+                error: "Invalid username or password", 
+                person: {
+                    username: this.state.person['username'],
+                    password: this.state.person['password']
+                }
+            };
+            
+            // Set state to new errored state
+            this.setState(errorState);
+        }
+        else {
+            console.log("when clicking, the form data is:");
+            console.log(this.state.person);
+        }
+/******************************************************************************/        
+    }
+
+}
+
+
+/**
+ * 
+ */
+function LoginHeader() {
+    return (
+        <div id="loginHeader">
+            <h1>Boston Code Camp</h1>
+            <h2>Administrator Login</h2>
+        </div>
+    );
+}
+
+/**
+ * A username text field
+ * 
+ * @param   {{
+ *              onChange: Function;
+ *          }} props 
+ */
+function UsernameField(props) {
+    return (
+        <TextField
+            name="username"
+            id="outlined-username-input"
+            label="Username"
+            margin="normal"
+            variant="outlined"
+            className="fullWidth"
+            onChange={ props.onChange }
+        />
+    );
+}
+
+/**
+ * A password text field
+ * 
+ * @param   {{
+ *              onChange: Function;
+ *          }} props 
+ */
+function PasswordField(props) {
+    return (
+        <TextField
+            name="password"
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            margin="normal"
+            variant="outlined"
+            className="fullWidth"
+            onChange={ props.onChange }
+        />
+    );
+}
+
+/**
+ * Displays error message
+ * 
+ * @param   {{
+ *              error: string
+ *          }} props 
+ */
+function ErrorMessage(props){
+    return (
+        <div id="errorContainer">
+            <p className="noPadding noMargin">
+                { props.error }
+            </p>
+        </div>
+    );
+}
---
+++ src/components/buttons.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import AddIcon from '@material-ui/icons/Add';
+
+/**
+ * 
+ * @param {{
+ *          onClick: function, 
+ *          className: string
+ *        }} props
+ */
+export function AddButton(props) {
+    return (
+        <Button
+            variant="contained"
+            startIcon={ <AddIcon /> }
+            onClick = { props.onClick }
+            className={ props.className }
+        >
+            Add
+        </Button>
+    );
+}
+
+/**
+ * 
+ * @param {{
+ *          onClick: function, 
+ *          className: string
+ *        }} props
+ */
+export function EditButton(props) {
+    return (
+        <Button
+            variant="outlined"
+            startIcon={ <EditIcon /> }
+            onClick = { props.onClick }
+            className={ props.className }
+        >
+            Edit
+        </Button>
+    );
+}
+
+/**
+ * 
+ * @param {{
+ *          onClick: function, 
+ *          className: string
+ *        }} props
+ */
+export function DeleteButton(props) {
+    return (
+        <Button
+            variant="contained"
+            startIcon={ <DeleteIcon /> }
+            onClick = { props.onClick }
+            className={ props.className }
+        >
+            Delete
+        </Button>
+    );
+}
+
+/**
+ * 
+ * @param {{
+ *          onClick: function, 
+ *          className: string
+ *        }} props
+ */
+   export function LoginButton(props) {
+       return (
+           <Button
+               variant="contained"
+               onClick = { props.onClick }
+               className={ props.className }
+           >
+               Login
+           </Button>
+       );
+   }
